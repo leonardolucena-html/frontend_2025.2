11d3d9455a24272fdf6ed67c9c13d412b3e3d672
leonardolucena-html <leonardo.lucena@alpha.edu.br>
2025-08-28 12:32:08 -0300

    feat: Add funões básicas

---
+++ funcoes.js
@@ -0,0 +1,20 @@
+let resposta = 'Leonardo' //window.confirm("Você estuda Frontend?");
+
+let processamento = ''
+
+if(resposta){
+    processamento = 'Sim'
+}else{
+    processamento = 'Não'
+}
+
+document.getElementById("resposta").innerHTML = processamento;
+let resultado = (4+4)%2 == 0
+let somaPar =  resultado ? 'Par' : 'Ímpar';
+
+for(let i = 0; i < 10; i++){
+    //console.log(i);
+}
+
+console.log(5 === 5);
+console.log(5 === '5');
